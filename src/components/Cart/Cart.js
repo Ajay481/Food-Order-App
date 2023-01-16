f3fdbf8896fde7a9445579ec02bc07ec53e97acd
Ajay481 <iamajayyadav481@gmail.com>
2023-01-16 13:34:51 +0530

    + & - button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,30 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartContx = useContext(CartContext);
+
+  const cartItemRemoveHandler = (id) => {
+    cartContx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartContx.addItem(item);
+  };
   const cartItems = (
-    <ul>
+    <ul className={classes["cart-items"]}>
       {cartContx.items.map((item) => (
-        <ul>
-          Name:{item.name} Price:{item.price} Quantity:{item.quantity}
-        </ul>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          quantity={item.quantity}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
